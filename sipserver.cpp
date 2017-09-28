f0a7538772a8c0a1cc82a225d7af723e7410ccf9
Andrea Galbusera <gizero@gmail.com>
2017-09-28 11:27:01 +0200

    add SipServer class
    This class implements a Websocket server with trivial echo capabilities

---
+++ sipserver.cpp
@@ -0,0 +1,123 @@
+/****************************************************************************
+**
+** Copyright (C) 2017 Andrea Galbusera <gizero@gmail.com>.
+**
+** $QT_BEGIN_LICENSE:BSD$
+**
+** BSD License Usage
+** Alternatively, you may use this file under the terms of the BSD license
+** as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+#include "sipserver.h"
+#include "QtWebSockets/qwebsocketserver.h"
+#include "QtWebSockets/qwebsocket.h"
+#include <QtCore/QDebug>
+
+QT_USE_NAMESPACE
+
+//! [constructor]
+SipServer::SipServer(quint16 port, bool debug, QObject *parent) :
+    QObject(parent),
+    m_pWebSocketServer(new QWebSocketServer(QStringLiteral("SIP Server"),
+                                            QWebSocketServer::NonSecureMode, this)),
+    m_debug(debug)
+{
+    if (m_pWebSocketServer->listen(QHostAddress::Any, port)) {
+        if (m_debug)
+            qDebug() << "SipServer listening on port" << port;
+        connect(m_pWebSocketServer, &QWebSocketServer::newConnection,
+                this, &SipServer::onNewConnection);
+        connect(m_pWebSocketServer, &QWebSocketServer::closed, this, &SipServer::closed);
+    }
+}
+//! [constructor]
+
+SipServer::~SipServer()
+{
+    m_pWebSocketServer->close();
+    qDeleteAll(m_clients.begin(), m_clients.end());
+}
+
+//! [onNewConnection]
+void SipServer::onNewConnection()
+{
+    QWebSocket *pSocket = m_pWebSocketServer->nextPendingConnection();
+
+    connect(pSocket, &QWebSocket::textMessageReceived, this, &SipServer::processTextMessage);
+    connect(pSocket, &QWebSocket::binaryMessageReceived, this, &SipServer::processBinaryMessage);
+    connect(pSocket, &QWebSocket::disconnected, this, &SipServer::socketDisconnected);
+
+    m_clients << pSocket;
+}
+//! [onNewConnection]
+
+//! [processTextMessage]
+void SipServer::processTextMessage(QString message)
+{
+    QWebSocket *pClient = qobject_cast<QWebSocket *>(sender());
+    if (m_debug)
+        qDebug() << "Message received:" << message;
+    if (pClient) {
+        pClient->sendTextMessage(message);
+    }
+}
+//! [processTextMessage]
+
+//! [processBinaryMessage]
+void SipServer::processBinaryMessage(QByteArray message)
+{
+    QWebSocket *pClient = qobject_cast<QWebSocket *>(sender());
+    if (m_debug)
+        qDebug() << "Binary Message received:" << message;
+    if (pClient) {
+        pClient->sendBinaryMessage(message);
+    }
+}
+//! [processBinaryMessage]
+
+//! [socketDisconnected]
+void SipServer::socketDisconnected()
+{
+    QWebSocket *pClient = qobject_cast<QWebSocket *>(sender());
+    if (m_debug)
+        qDebug() << "socketDisconnected:" << pClient;
+    if (pClient) {
+        m_clients.removeAll(pClient);
+        pClient->deleteLater();
+    }
+}
+//! [socketDisconnected]
+
+int SipServer::getClientsCount()
+{
+    return m_clients.count();
+}
