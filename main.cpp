c94ec3c64d1ea5ce8485c949e61aa0f9b8e04493
Andrea Galbusera <gizero@gmail.com>
2017-09-28 11:49:55 +0200

    add SipServer instance to main app

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include "mainwindow.h"
+#include "sipserver.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    bool debug = true;
+    int port = 12345;
+
     QApplication a(argc, argv);
+
+    SipServer *ss = new SipServer(port, debug);
+    QObject::connect(ss, &SipServer::closed, &a, &QApplication::quit);
+
     MainWindow w;
     w.show();
 
