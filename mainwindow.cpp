b52ae12ac4c6a484a10eb28ba79aed6c1fbcc0a4
Andrea Galbusera <gizero@gmail.com>
2017-09-28 15:15:27 +0200

    mainwindow: add SipNotifier instance to LineEdit(s)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "sipnotifier.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    SipNotifier *sipNotifier = new SipNotifier(this);
+    ui->lineEdit->installEventFilter(sipNotifier);
+    ui->lineEdit_2->installEventFilter(sipNotifier);
 }
 
 MainWindow::~MainWindow()
