2303a81dc5396e99abc34e8147fb012b30657c90
Andrea Galbusera <gizero@gmail.com>
2017-10-13 16:29:38 +0200

    sipnotifier: include QlineEdit text to message sent to clients

--- sipnotifier.cpp
+++ sipnotifier.cpp
@@ -1,4 +1,5 @@
 #include "sipnotifier.h"
+#include <QLineEdit>
 #include <QtCore/QDebug>
 
 SipNotifier::SipNotifier(QObject *parent, SipServer *server) : QObject(parent)
@@ -11,8 +12,12 @@ bool SipNotifier::eventFilter(QObject *watched, QEvent *event)
     if (event->type() == QEvent::FocusIn)
     {
         qDebug() << "someone got focus!";
+
+        // This is ugly but for now we only watch QLineEdit instances
+        QString wt = static_cast<QLineEdit *>(watched)->text();
+
         if (m_server)
-            m_server->sendBcastTextMessage("{ \"type\": \"EDITMODE\", \"text\": \"hello!\" }");
+            m_server->sendBcastTextMessage("{ \"type\": \"EDITMODE\", \"text\": \"" + wt + "\" }");
 //        if (watched == ui->lineEdit)
 //        {
 //            qDebug() << "lineEdit got focus!";
