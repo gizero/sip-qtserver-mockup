473b27ef7daae1ea7a3862ef8d23d2bdfdfc0d24
Andrea Galbusera <gizero@gmail.com>
2017-10-16 06:59:39 +0200

    add Dockerfile for test container
    This container runs x11vnc on xvfb and serves the sip-qtserver-mockup
    test application.

---
+++ docker/image/startup.sh
@@ -0,0 +1,10 @@
+if [ -n "$VNC_PASSWORD" ]; then
+    echo -n "$VNC_PASSWORD" > /.password1
+    x11vnc -storepasswd $(cat /.password1) /.password2
+    rm /.password1
+    chmod 400 /.password2
+    sed -i 's/^command=x11vnc.*/& -rfbauth \/.password2/' /etc/supervisor/conf.d/supervisord.conf
+    export VNC_PASSWORD=
+fi
+
+exec /usr/bin/supervisord -n
