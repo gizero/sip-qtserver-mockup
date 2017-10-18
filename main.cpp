c75601e6aa7f9f1a12d91bbf4d567ff4ccb649db
Andrea Galbusera <gizero@gmail.com>
2017-10-18 07:06:22 +0200

    main: allow passing server port from command line

--- main.cpp
+++ main.cpp
@@ -14,7 +14,17 @@ int main(int argc, char *argv[])
     parser.setApplicationDescription("Qt server mockup");
     parser.addHelpOption();
 
+    QCommandLineOption portOption(QStringList() << "p" << "port",
+                                   "TCP port to listen to (1-65535).", "12345");
+    parser.addOption(portOption);
+
     parser.process(a);
+
+    port = parser.value(portOption).toInt();
+    if (port < 1 || port > 65535) {
+        fprintf(stderr, "%s\n", qPrintable(QCoreApplication::translate("main", "Error: Invalid port argument. Must be 1-65535")));
+                parser.showHelp(1);
+    }
 
     SipServer *ss = new SipServer(port, debug);
     QObject::connect(ss, &SipServer::closed, &a, &QApplication::quit);
